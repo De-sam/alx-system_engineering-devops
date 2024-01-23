cd7dc3cc8a2b7f4657852f5d395bee6fc0ae48da
De-sam <samsoncoded@gmail.com>
2024-01-23 21:25:34 +0100

    server

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -5,3 +5,10 @@ package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
 }
+
+# adding directory to PATH
+exec { 'update_path':
+  command     => 'export PATH=$PATH:/path/to/directory/containing/flask',
+  path        => ['/bin', '/usr/bin', '/usr/local/bin'],
+  refreshonly => true,
+}
